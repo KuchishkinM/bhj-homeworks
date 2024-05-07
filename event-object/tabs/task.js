789c54b9e762a2e641f10b971cb1c6ff1540b26f
Maksim Kuchishkin <kuchishkin.m@gmail.com>
2024-05-07 12:25:20 +0300

    second homework

---
+++ event-object/tabs/task.js
@@ -0,0 +1,16 @@
+const tabs = Array.from(document.getElementsByClassName('tab'));
+const contents = Array.from(document.getElementsByClassName('tab__content'));
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', function () {
+        let index = tabs.indexOf(tab)
+        for (let i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove("tab_active");
+        };
+        tab.classList.add('tab_active');
+        for (let j = 0; j < contents.length; j++) {
+            contents[j].classList.remove('tab__content_active')
+        };
+        contents[index].classList.add('tab__content_active');
+    });
+});

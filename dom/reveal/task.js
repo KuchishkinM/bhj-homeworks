51e7aecfeeb8070dab186df87854528e44c63823
Maksim Kuchishkin <kuchishkin.m@gmail.com>
2024-05-09 09:10:07 +0300

    fix second task

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,9 +1,15 @@
-const reveal = document.querySelector('div.reveal');
+const reveals = document.querySelectorAll('.reveal');
 const viewportHeight = window.innerHeight;
-const revalTop = reveal.getBoundingClientRect().top;
 
-if (viewportHeight > revalTop) {
-    reveal.classList.add('reveal_active')
+const scrollFu = () => {
+  for (let i = 0; i < reveals.length; i++) {
+    const revealTop = reveals[i].getBoundingClientRect().top;
+    if (revealTop < viewportHeight) {
+      reveals[i].classList.add('reveal_active');
+    } else {
+      reveals[i].classList.remove('reveal_active');
+    };
+  };
 };
 
-
+document.addEventListener('scroll', scrollFu);
